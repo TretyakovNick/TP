376900458b1843a1dd5b1c5a7f6b219b6ac29489
Николай <nmtretyakov@mail.ru>
2019-04-08 21:41:28 +0300

    Erased all

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/Codes/TP
-# Build directory: /home/nick/Codes/TP/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("googletest/googletest-build")
-subdirs("test")
